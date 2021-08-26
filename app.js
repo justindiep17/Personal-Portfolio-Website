fcbc0d683f7abe6af398589128483e978ed7bd1b
OnlyDill <justindiep17@gmail.com>
2021-08-26 00:22:40 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.urlencoded({ extended: true }))
+
+// Homepage
+app.get('/', (req, res) => {
+  res.render('home.ejs')
+})
+
+// Contact Page
+app.get('/contact', (req, res) => {
+  res.render('contact.ejs')
+})
+
+app.listen(3000, () => {
+  console.log("ON PORT 3000!")
+})
