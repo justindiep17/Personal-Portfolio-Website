c076abc83deeb85bda5982b6ec474cb4ad5bc2c1
OnlyDill <justindiep17@gmail.com>
2021-09-18 03:09:55 -0400

    fixed typo in projects page

--- app.js
+++ app.js
@@ -1,82 +1,83 @@
-const express = require('express')
-const nodemailer = require('nodemailer')
-const ejs = require('ejs')
-const path = require('path')
+const express = require("express");
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const path = require("path");
 
-const app = express()
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '/views'))
-app.use(express.static(path.join(__dirname, '/public')))
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
     user: process.env.SITE_EMAIL,
-    pass: process.env.EMAIL_PASSWORD
-  }
-})
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 // Homepage
-app.get('/', (req, res) => {
-  res.render('home.ejs')
-})
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
 
 // Contact Page
-app.get('/contact', (req, res) => {
-  res.render('contact.ejs')
-})
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
 
-app.post('/contact', (req, res) => {
-  const { name, email, msg } = req.body
+app.post("/contact", (req, res) => {
+  const { name, email, msg } = req.body;
   const mailOptions = {
-    from: 'onlydill202@gmail.com',
-    to: 'justindiep17@gmail.com',
-    subject: 'Contact Form Submission',
-    text: `Name: ${name}\nEmail:${email}\nMessage:${msg}`
-  }
+    from: "onlydill202@gmail.com",
+    to: "justindiep17@gmail.com",
+    subject: "Contact Form Submission",
+    text: `Name: ${name}\nEmail:${email}\nMessage:${msg}`,
+  };
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      console.log(error)
+      console.log(error);
     } else {
-      console.log('Email sent: ' + info.response)
+      console.log("Email sent: " + info.response);
     }
-  })
-  res.redirect('/')
-})
+  });
+  res.redirect("/");
+});
 
 // Projects Page
 
-app.get('/projects', (req, res) => {
-  const rootPath = "/imgs/projects/"
-  res.render('projects.ejs', {
+app.get("/projects", (req, res) => {
+  const rootPath = "/imgs/projects/";
+  res.render("projects.ejs", {
     projects: [
       {
         img_path: rootPath + "roll_it.jpg",
-        name: "Simple Die",
-        description: "Cross platform mobile app that provides a simple yet elegant way to roll dies digitally.",
+        name: "Simple Dice",
+        description:
+          "Cross platform mobile app that provides a simple yet elegant way to roll dies digitally.",
         link: "#",
-        attributes: ["Dart", "Flutter"]
+        attributes: ["Dart", "Flutter"],
       },
       {
         img_path: rootPath + "the_hearth.png",
         name: "The Hearth",
-        description: "Fully functional blog website about Blizzard's" + ' popular video game Hearthstone',
+        description:
+          "Fully functional blog website about Blizzard's" +
+          " popular video game Hearthstone",
         link: "#",
-        attributes: ["Python", "Flask", "HTML/CSS", "JavaScript"]
+        attributes: ["Python", "Flask", "HTML/CSS", "JavaScript"],
       },
       {
         img_path: rootPath + "busy.png",
         name: "IsItBusy?",
-        description: "Web app that checks the busyness of gorcery stores based on crowd sourced data.",
+        description:
+          "Web app that checks the busyness of gorcery stores based on crowd sourced data.",
         link: "#",
-        attributes: ["Python", "Flask", "HTML/CSS", "MongoDB",]
+        attributes: ["Python", "Flask", "HTML/CSS", "MongoDB"],
       },
-    ]
-  })
-})
+    ],
+  });
+});
 
-
-app.listen(process.env.PORT || 5000, () => {
-})
-
+app.listen(process.env.PORT || 5000, () => {});
